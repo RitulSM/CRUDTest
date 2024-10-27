8c80b4043a759ea53cc5950cfc59dcbde151dbb1
Ritul <01fe22bca126@kletech.ac.in>
2024-10-27 10:51:29 +0530

    Logger messages added

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 import express from 'express';
 const app = express();
 const port = 5000;
+import logger from "./Logger.js";
+import morgan from "morgan";
 
 app.use(express.json());
 let teaData = [];
 let nextId = 1;
-
+const morganFormat = ":method :url :status :response-time ms";
+app.use(
+    morgan(morganFormat, {
+      stream: {
+        write: (message) => {
+          const logObject = {
+            method: message.split(" ")[0],
+            url: message.split(" ")[1],
+            status: message.split(" ")[2],
+            responseTime: message.split(" ")[3],
+          };
+          logger.info(JSON.stringify(logObject));
+        },
+      },
+    })
+  );
 app.post('/coffees', (req, res) => {
     const { name, price } = req.body;
     const newTea = { id: nextId++, name, price };
